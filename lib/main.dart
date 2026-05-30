f8053f8eb9defbcec1bf789e6d734d11221ff11f
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-29 22:49:50 -0500

    show homepage

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,5 @@ import 'package:travellappg15/pages/home_page.dart';
 import 'package:travellappg15/pages/welcome_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(home: SharedPrefencesPage(), debugShowCheckedModeBanner: false),
-  );
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
 }
